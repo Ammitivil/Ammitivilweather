7e85c3ccdd1725356bc1739e83a11f11575a6f02
Ammitivil <ammitivil@gmail.com>
2024-05-20 12:34:04 +0900

    Изначально использовал не работающую библиотеку, заменил на telebot!!!

--- Ammitivilweather.py
+++ Ammitivilweather.py
@@ -1,15 +1,25 @@
-import os
-import datetime
-import requests
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor 
-bot = Bot(token='@Ammitivilweather_bot')
-dp = Dispatcher(bot)
-@dp.message.handler(commands=["start"])
-async def start_command(message: types.Message):
-        await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды")
-if __name__ == "__main__":
-        # С помощью метода executor.start_polling опрашиваем
-    # Dispatcher: ожидаем команду /start
-        executor.start_polling(dp)
+# подключаем модуль для Телеграма
+import telebot
+
+# указываем токен для доступа к боту
+bot = telebot.TeleBot('6888460713:AAEGF1AbX_1toFc9a0weHtF0d9_T36j8JPI')
+
+# приветственный текст
+start_txt = 'Привет! Напиши свой город. \n И я тебе покажу прогноз погоды.'
+
+
+# обрабатываем старт бота
+@bot.message_handler(commands=['start'])
+def start(message):
+    # выводим приветственное сообщение
+    bot.send_message(message.from_user.id, start_txt, parse_mode='Markdown')
+
+# запускаем бота
+if __name__ == '__main__':
+    while True:
+        # в бесконечном цикле постоянно опрашиваем бота — есть ли новые сообщения
+        try:
+            bot.polling(none_stop=True, interval=0)
+        # если возникла ошибка — сообщаем про исключение и продолжаем работу
+        except Exception as e: 
+            print('❌❌❌❌❌ Сработало исключение! ❌❌❌❌❌')
